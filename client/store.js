b4ba6953d8414e6e365f6b4e5abeb460b54deb79
RonyVidaur <rony.vidaur@gmail.com>
2016-11-26 22:37:19 -0600

    redux store to handle data

---
+++ client/store.js
@@ -0,0 +1,17 @@
+import { createStore, compose } from 'redux'
+import { syncHistoryWithStore } from 'react-router-redux'
+import { browserHistory } from 'react-router'
+
+//import the root reducer
+import rootReducer from './reducers/index'
+
+
+import React, { PropTypes } from 'react'
+
+class  extends React.Component {
+  render () {
+
+  }
+}
+
+export default
