e70b55a65a596a48078fa9383aa5533f02c1df6d
RonyVidaur <rony.vidaur@gmail.com>
2016-12-28 23:25:29 -0600

    minor refactoring

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,22 +1,21 @@
 function postComments(state = [], action) {
-switch (action.type) {
-  case 'ADD_COMMENT':
-    return[...state,{
-      user: action.author,
-      text: action.comment
-    }]
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      return[...state,{
+        user: action.author,
+        text: action.comment
+      }]
     
-
-  case 'REMOVE_COMMENT':
-    return [
-      ...state.slice(0, action.i),
-      ...state.slice(action.i + 1)
-    ]
-
-
-  default:
-  return state
-  }
+    case 'REMOVE_COMMENT':
+      return [
+        ...state.slice(0, action.i),
+        ...state.slice(action.i + 1)
+      ]
+    
+    default:
+    return state
+    }
+  
 }
 
 function comments (state = [], action) {
@@ -31,4 +30,4 @@ function comments (state = [], action) {
 }
 
 
-export default comments
+  export default comments
